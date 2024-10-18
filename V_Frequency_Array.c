66d25a5bfd3a896bc96faf6d530b4f232d776c2c
nabil0203 <nabilahmed0203@gmail.com>
2024-10-18 22:47:02 +0600

    update

---
+++ V_Frequency_Array.c
@@ -0,0 +1,34 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/V
+
+#include <stdio.h>
+int main()
+{
+
+    int n, m, i;
+    scanf("%d %d", &n, &m);
+
+    int a[n];
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 1; i <= m; i++)
+    {
+        int count = 0;
+
+        if (a[i] >= 1 && a[i] <= m)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (a[i] == i)
+                {
+                    count++;
+                }
+            }
+        }
+        printf("%d\n", count);
+    }
+
+    return 0;
+}
